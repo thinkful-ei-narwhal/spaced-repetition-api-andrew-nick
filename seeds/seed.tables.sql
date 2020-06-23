414c5c6b8fd7c54c8bf10d346b6d7ea6cebe41f7
Andrew Durrant <andrew.durrant75@gmail.com>
2020-06-22 19:33:44 -0600

    deploy to heroku

--- seeds/seed.tables.sql
+++ seeds/seed.tables.sql
@@ -23,14 +23,14 @@ INSERT INTO "word" ("id", "language_id", "original", "translation", "next")
 VALUES
   (1, 1, 'donde esta el bano', 'where is the bathroom', 2),
   (2, 1, 'disculpe', 'excuse me', 3),
-  (3, 1, 'estoy perdido', 'I\'m lost', 4),
+  (3, 1, 'estoy perdido', 'I am lost', 4),
   (4, 1, 'aqui', 'here', 5),
   (5, 1, 'una bebida', 'a drink', 6),
   (6, 1, 'cuanto', 'how much', 7),
   (7, 1, 'por cuanto tiempo', 'how long', 8),
   (8, 1, 'por favor', 'please', 9),
   (9, 1, 'como esta', 'how are you', 10),
-  (10, 1, 'como te llamas', 'what\'s your name', null);
+  (10, 1, 'como te llamas', 'what is your name', null);
 
 UPDATE "language" SET head = 1 WHERE id = 1;
 
